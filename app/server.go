54dd5e8251c9630078c2fb63f86b0a3de3e84758
Clay Karwisch <clay.karwisch@gmail.com>
2024-11-27 08:27:15 -0500

    implement echo command and refactor

--- app/server.go
+++ app/server.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -26,13 +27,47 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-	s := bufio.NewScanner(conn)
-	for s.Scan() {
-		line := s.Text()
-		fmt.Println("received: ", line)
-
-		if line == "PING" {
-			conn.Write([]byte("+PONG\r\n"))
+	for {
+		buf := make ([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("Error reading: ", err.Error())
 		}
+
+		if n < 2 {
+			fmt.Println("Breaking...")
+			break
+		}
+
+		fmt.Println("read n bytes: ", n)
+		fmt.Println("received: ", string(buf[:n]))
+
+		switch buf[0] {
+			case byte('*'):
+				fmt.Println("received array")
+				handleArray(buf, n, conn)
+			default:
+				fmt.Println("received unsupported request")
+		}
+	}
+}
+
+func handleArray(buf []byte, n int, conn net.Conn) {
+	arrayLength, _ := strconv.Atoi(string(buf[1]))
+	cmdLength, _ := strconv.Atoi(string(buf[5]))
+	command := buf[8:8+cmdLength]
+
+	switch {
+		case bytes.EqualFold(command, []byte("ping")):
+			fmt.Println("matched ping")
+			conn.Write([]byte("+PONG\r\n"))
+			if arrayLength > 1 {
+				buf = append([]byte{}, buf[0:5]...)
+				buf = append(buf, buf[5+cmdLength+2:]...)
+				handleArray(buf, n, conn)
+			}
+		case bytes.EqualFold(command, []byte("echo")):
+			fmt.Println("matched echo")
+			conn.Write(buf[7+int(cmdLength+3):n])
 	}
 }
